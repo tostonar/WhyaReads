1b125e9a9c821dfe550e28a28dceec1628f67302
Andrea T <tostonar@gmail.com>
2019-10-01 16:51:45 -0400

    Form for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,16 @@
 class ReviewsController < ApplicationController
+  before_action :find_book
   def new
     @review = Review.new
   end
 
   def create
+    byebug
     @review = Review.new(review_params)
+    @review.book_id = @book.id
+    @review.user_id = current_user.id
     if @review.save
-      redirect_to new_book_review(@review)
+      redirect_to book_path(@book)
     else
       render :new
     end
@@ -29,6 +33,10 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:comment, :rating, :user_id, :book_id)
+  end
+
+  def find_book
+    @book = Book.find(params[:book_id])
   end
 
 end

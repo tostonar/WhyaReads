87a73ee81af5caf7ea1b62bdab10cadbac7e4fea
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-01 12:49:33 -0400

    added new form to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,19 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
   def show
   end
 
@@ -12,4 +25,9 @@ class BooksController < ApplicationController
   def find_book
     @book = Book.find_by(id: params[:id])
   end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :purchase_url, :image_url)
+  end
+  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author
+  validates :title, uniqueness: {case_sensitive: false}
+  validates :author, presence: true
+  
 end

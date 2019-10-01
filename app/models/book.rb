449eb6293e883c5db723b4a5e37a6c0f2834c32a
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-01 15:25:43 -0400

    review on book show page

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :author
+  has_many :reviews
   validates :title, uniqueness: {case_sensitive: false}
   validates :author, presence: true
   validates :category, presence: true

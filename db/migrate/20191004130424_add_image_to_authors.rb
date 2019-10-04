09409fc305af14cf20c614c08ab3680ecf5625f9
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-04 09:08:10 -0400

    added image column to authors table

---
+++ db/migrate/20191004130424_add_image_to_authors.rb
@@ -0,0 +1,5 @@
+class AddImageToAuthors < ActiveRecord::Migration[5.2]
+  def change
+    add_column :authors, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_181111) do
+ActiveRecord::Schema.define(version: 2019_10_04_130424) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "books", force: :cascade do |t|
